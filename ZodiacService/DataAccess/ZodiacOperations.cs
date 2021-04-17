4bc6b1f38483a3fc2d69188c6857733b3ac87b4c
Valent <80772449+Vlad-Valentin@users.noreply.github.com>
2021-04-17 16:23:30 +0300

    Added ZodiacService
    Added folder for ZodiacService.
    Added class for reading a .txt file.
    Added .txt file for zodiac signs.

---
+++ ZodiacService/DataAccess/ZodiacOperations.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZodiacService.DataAccess
+{
+    public class ZodiacOperations
+    {
+        private const string FilePath = "./Resources/signs.txt";
+
+        public static List<Tuple<Zodiac, Zodiac, string>> GetAllZodiacs()
+        {
+            //var zodiacs = new List<Zodiac>();
+
+            var zodiacs = new List<Tuple<Zodiac, Zodiac, string>>();
+
+            try
+            {
+                var sr = new StreamReader(FilePath);
+                var line = sr.ReadLine()?.Split(" ");
+                while (line != null)
+                {
+                    zodiacs.Add(new Tuple<Zodiac, Zodiac, string>(
+                        new Zodiac() { Date = line[0] },
+                        new Zodiac() { Date = line[1] },
+                        line[2]));
+
+                    line = sr.ReadLine()?.Split(" ");
+                }
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+
+            return zodiacs;
+        }
+    }
+}
